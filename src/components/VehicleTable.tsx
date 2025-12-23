2a09f22af4cfab32d8df68ed394240d4a032603b
volkan mutaf <mutaf@usa.com>
2025-12-23 18:39:50 -0500

    Complete UI improvements: lighter colors, remove Pre-Insp text, smaller icons

--- src/components/VehicleTable.tsx
+++ src/components/VehicleTable.tsx
@@ -2166,7 +2166,6 @@ ${mileage}`
                   className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500 cursor-pointer"
                   title="Pre-inspection completed"
                 />
-                <span className="text-xs text-gray-700 font-medium">Pre-Inspection</span>
               </label>
             </div>
             
