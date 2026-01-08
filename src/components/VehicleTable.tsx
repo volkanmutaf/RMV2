70a36da65556f9c0477e209999dff0592c9aa397
volkan mutaf <mutaf@usa.com>
2026-01-08 15:59:33 -0500

    Cleanup duplicate keys in Quick Add and trigger deploy

--- src/components/VehicleTable.tsx
+++ src/components/VehicleTable.tsx
@@ -409,8 +409,6 @@ ${mileage}`
           status: null,
           plate: '',
           note: '',
-          plate: '',
-          note: '',
           ref: editableData.ref || '',
           date: new Date()
         }
